f10a73f579afc1f9c20e995fbb34bd1331672793
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-02-08 13:18:36 -0600

    miner running/first coin mined succesfully

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -2,6 +2,7 @@ import hashlib
 import requests
 
 import sys
+import json
 
 from uuid import uuid4
 
@@ -25,12 +26,14 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
 
-def valid_proof(last_hash, proof):
+def valid_proof(last_proof, proof):
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
     the hash of the last proof match the first six characters of the hash
@@ -40,7 +43,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    last_hash = hashlib.sha256(f'{last_proof}'.encode()).hexdigest()
+    curr_hash= hashlib.sha256(f'{proof}'.encode()).hexdigest()
+    return curr_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
