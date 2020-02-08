e4d0d74511be6ee81cae89cfd8e60bdc48355f83
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-02-08 12:32:58 -0600

    ex2 tests passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,23 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    for ticket in tickets:
+        #insert every ticket
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+        """
+        The ticket with the none for source will be the first ticket
+        and will start the ticket chain as the first current ticket
+        """
+        if ticket.source == "NONE":
+            route[0] = ticket.destination
+    
+    current_ticket = route[0]
+    ticket_pointer = 1
+    
+    # increment through the linked list and proceed using  current_ticket and ticket_pointer
+    while route[-1] is None:
+        route[ticket_pointer] = hash_table_retrieve(hashtable, current_ticket)
+        current_ticket = route[ticket_pointer]
+        ticket_pointer += 1
 
     return route
